0b9ea2cd1c0670f8216c054a243d7f6a0716ffe1
SociallyAwkwardNerd <ghassenbessrour@gmail.com>
2019-02-24 23:32:41 -0500

    fib]

--- C/fib.c
+++ C/fib.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void main()
-{
+int fib(int);
 
-}
+void main()
+{   
+    int num, i = 0, c;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    for(c = 1; c <= num; c++)
+    {
+        printf("%d ", fib(i));
+        i++;
+    }
+}   
 
 int fib(int n)
 {
-    return (n-1) + (n-2);
+    if(n == 0 || n == 1)
+        return n;
+    else 
+        return fib(n-1) + fib(n-2);
 }
